880500a10abebfd1c4b323223277244d90802e73
Grishin Maxim <maxjmagic@gmail.com>
2025-03-28 15:41:48 +0300

    some updates

--- src/app/chat/message-form/message-form.component.ts
+++ src/app/chat/message-form/message-form.component.ts
@@ -4,7 +4,7 @@ import { MatFormFieldModule } from '@angular/material/form-field'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 
-import { EnterSubmitDirective } from '../../shared/EnterSubmit.directive';
+import { EnterSubmitDirective } from '../../shared/directives/EnterSubmit.directive';
 
 @Component({
   selector: 'app-message-form',
