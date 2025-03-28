50fffcbcdf1fb062629d114a29e40351acd1cded
Grishin Maxim <maxjmagic@gmail.com>
2025-03-28 10:57:41 +0300

    added submitMessage

--- src/app/chat/message-form/message-form.component.ts
+++ src/app/chat/message-form/message-form.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { MessageInputComponent } from "./message-input/message-input.component";
-import { MessageButtonComponent } from "./message-button/message-button.component";
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
+import { EnterSubmitDirective } from '../../shared/EnterSubmit.directive';
 
 @Component({
   selector: 'app-message-form',
-  imports: [ MessageInputComponent, MessageButtonComponent ],
+  imports: [ MatInputModule, MatFormFieldModule, MatButtonModule, FormsModule, ReactiveFormsModule, EnterSubmitDirective ],
   templateUrl: './message-form.component.html',
   styleUrl: './message-form.component.css'
 })
 export class MessageFormComponent {
+  chatMessage = '';
 
+  submitMessage() {
+    console.log('Отправлено:', this.chatMessage);
+    this.chatMessage = '';
+  }
 }
