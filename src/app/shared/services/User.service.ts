b57cfd1aa516d502fdfdb1606bd50d30daa00320
Grishin Maxim <maxjmagic@gmail.com>
2025-03-28 21:32:49 +0300

    Now you can open different tabs and each time the user name will be requested, thanks to SessionStorage

--- src/app/shared/services/User.service.ts
+++ src/app/shared/services/User.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
   private readonly USERNAME_KEY = 'chat_username';
+  private readonly SESSION_FLAG = 'active_session';
 
   getUsername(): string | null {
     return localStorage.getItem(this.USERNAME_KEY);
@@ -12,9 +13,10 @@ export class UserService {
 
   setUsername(username: string): void {
     localStorage.setItem(this.USERNAME_KEY, username);
+    sessionStorage.setItem(this.SESSION_FLAG, 'true');
   }
 
   hasUsername(): boolean {
-    return this.getUsername() !== null;
+    return !!this.getUsername() && !!sessionStorage.getItem(this.SESSION_FLAG);
   }
 }
