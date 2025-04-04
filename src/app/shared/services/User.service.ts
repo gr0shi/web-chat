5629bc084a9beddedc7bb5a4f3ab5370e1a5cf89
Grishin Maxim <maxjmagic@gmail.com>
2025-04-04 18:55:36 +0300

    Revert "fixed error: 'ReferenceError: localStorage is not defined'"
    This reverts commit 69c23ada7c9982ed24e2e8ff1017c04f5d48092c.

--- src/app/shared/services/User.service.ts
+++ src/app/shared/services/User.service.ts
@@ -1,28 +1,19 @@
-import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { Injectable } from '@angular/core';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private readonly USERNAME_KEY = 'chat_username';    // Ключ для хранения пользователей в localStorage
 
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: any
-  ) {}
-
   // Получает имя пользователя из localStorage
   getUsername(): string | null {
-    if (isPlatformBrowser(this.platformId)) {
-      return localStorage.getItem(this.USERNAME_KEY);
-    }
-    return null;
+    return localStorage.getItem(this.USERNAME_KEY);
   }
 
   // Устанавливает имя пользователя в localStorage
   setUsername(username: string): void {
-    if (isPlatformBrowser(this.platformId)) {
-      localStorage.setItem(this.USERNAME_KEY, username);
-    }
+    localStorage.setItem(this.USERNAME_KEY, username);
   }
 
   // Проверяет, есть ли пользователь в localStorage
